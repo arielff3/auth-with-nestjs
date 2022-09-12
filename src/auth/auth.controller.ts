7ffd346ee68ce34c798ba144cd56e464540188fe
arielff3 <ariel.franco.ferreira@hotmail.com>
2022-09-12 18:45:48 -0300

    :fire:

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+  Body,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth-guard';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+  constructor(private readonly service: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  async login(@Body() body) {
+    return this.service.validateUser(body);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+  async validateUser({ email, password }: { email: string; password: string }) {
+    const user = await this.userService.findByEmail({ email });
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (isPasswordValid) {
+        delete user.password;
+        return user;
+      }
+    }
+    throw new Error('Email address or password provided is incorrect.');
+  }
+}
