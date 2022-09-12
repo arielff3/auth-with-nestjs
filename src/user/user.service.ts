bd6ab01d8a59f9776a9dee9c4ed5f604527cadeb
arielff3 <ariel.franco.ferreira@hotmail.com>
2022-09-12 17:46:16 -0300

    :fire:

---
+++ src/user/user.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+import { GetUserDTO } from './dto/get-user.dto';
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const data = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    const createdUser = await this.prisma.user.create({
+      data,
+      select: {
+        name: true,
+        id: true,
+        email: true,
+      },
+    });
+
+    return createdUser;
+  }
+
+  async findByEmail({ email }: GetUserDTO) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return user;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
